64febf4961dd7c5f5de720c7a5caacce120aba08
Riten <riten.github@gmail.com>
2018-08-20 00:19:02 +0530

    code refactored

--- src/BookData.js
+++ src/BookData.js
@@ -3,51 +3,49 @@ import React from 'react';
 const Bookdata=(props)=>{
     
     const {booksArr, onMoveTo,catType,bookshelfdata}= props
+
+    let shelfArr=[{key:"currentlyReading",value:"Currently Reading"},
+                  {key:"wantToRead",value:"Want to Read"},
+                  {key:"read",value:"Read"},
+                  {key:"none",value:"None"}];
     const getBookShelf=(id)=>{
             let bookShelf="none"
-            bookshelfdata.forEach((book)=>{
+            let books=bookshelfdata===undefined?booksArr:bookshelfdata
+            books.forEach((book)=>{
                 if(book.id===id)
                 {
                     bookShelf=  book.shelf;
-                   return;
+                   
+                   return bookShelf;
                     
                 }
             });
+          
         return bookShelf;
         
     
         }
-    const setSelectedValue=(selid)=>{
-        
-        let id=document.getElementById(selid);
-        console.log("index "+ id.selectedIndex);
-        for(let i=1;i<=4;i++)
-               {
-           let selctedText=id.options[i];
-           let val=selctedText.attributes[0].value
-           
-              selctedText.innerHTML=(i===id.selectedIndex)?"&#10004; "+val: val
-                   console.log("text "+ selctedText.text);
-               }
-    }
+       
         return(
+         
             <ol className="books-grid">
            { booksArr.map((book)=>(
-                 
+                
                     (book.shelf=== undefined?true:book.shelf===catType) &&(
-
+               
                <li  key={book.id}>
                         <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.thumbnail})`
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks===undefined?"":book.imageLinks.thumbnail})`
                             }}></div>
                             <div className="book-shelf-changer">
-                              <select onClick={()=>setSelectedValue(book.id)} value={book.shelf=== undefined?getBookShelf(book.id):book.shelf} id={book.id} onChange={()=>onMoveTo(book,book.id)}>
+                              <select value={book.shelf=== undefined?getBookShelf(book.id): book.shelf} id={book.id} onChange={()=>onMoveTo(book,book.id)}>
                                 <option  disabled>Move to...</option>
-                                <option name="Currently Reading" value="currentlyReading">Currently Reading</option>
-                                <option name="Want to Read"value="wantToRead">Want to Read</option>
-                                <option name="Read"value="read">Read</option>
-                                <option name="None"value="none">None</option>
+                                {
+                                    shelfArr.map((opt)=>{
+                                  return opt.key===(book.shelf=== undefined?getBookShelf(book.id): book.shelf)?<option key={opt.key+""+book.id} value={opt.key}>&#10004; {opt.value}</option>:<option key={opt.key+""+book.id} value={opt.key}>{opt.value}</option>
+                                })}
+                               
                               </select>
                             </div>
                           </div>
